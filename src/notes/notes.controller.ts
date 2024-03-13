9e42894950bb87ab359fe9a2a3f617775c738dd5
Wasik Ahmed Apon <apon.wasik2001@gmail.com>
2024-03-14 00:35:19 +0600

    Update controller for Note Lists and Notes.

--- src/notes/notes.controller.ts
+++ src/notes/notes.controller.ts
@@ -1,34 +1,46 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { NotesService } from './notes.service';
 import { CreateNoteDto } from './dto/create-note.dto';
 import { UpdateNoteDto } from './dto/update-note.dto';
+import { Note } from './entities/note.entity';
 
-@Controller('notes')
+@Controller('note-lists/notes')
 export class NotesController {
   constructor(private readonly notesService: NotesService) {}
 
   @Post()
-  create(@Body() createNoteDto: CreateNoteDto) {
-    return this.notesService.create(createNoteDto);
+  async create(@Body() createNoteDto: CreateNoteDto): Promise<Note> {
+    return await this.notesService.create(createNoteDto);
   }
 
   @Get()
-  findAll() {
-    return this.notesService.findAll();
+  async findAll(): Promise<Note[]> {
+    return await this.notesService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.notesService.findOne(+id);
+  async findOne(@Param('id') id: string): Promise<Note> {
+    return await this.notesService.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateNoteDto: UpdateNoteDto) {
-    return this.notesService.update(+id, updateNoteDto);
+  async update(
+    @Param('id') id: string,
+    @Body() updateNoteDto: UpdateNoteDto,
+  ): Promise<Note> {
+    return await this.notesService.update(+id, updateNoteDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.notesService.remove(+id);
+  async remove(@Param('id') id: string): Promise<void> {
+    return await this.notesService.remove(+id);
   }
 }
